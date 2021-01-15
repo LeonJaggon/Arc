cad5c86216e6ffa355104cafbf8fa303e36ee8e1
Leon Jaggon <ljaggon1@pride.hofstra.edu>
2021-01-14 19:31:06 -0500

    First commit.

---
+++ src/main/java/org/Arc/Web/WebController.java
@@ -0,0 +1,13 @@
+package org.Arc.Web;
+
+// import org.Arc.Web.Acuity.AcuityDriver;
+// import org.Arc.Web.Conex.ConexDriver;
+// import org.Arc.Web.Radius.RadiusDriver;
+
+public class WebController {
+    // private AcuityDriver acuityDriver;
+    // private RadiusDriver radiusDriver;
+    // private ConexDriver conexDriver;
+    
+    
+}
